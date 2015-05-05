300ff81675b73c8197ec34a634fc7994a650d219
Steven Traversi <straversi@berkeley.edu>
2015-05-05 13:57:58 -0700

    In working condition for lab testing

--- giants.py
+++ giants.py
@@ -21,6 +21,23 @@ def update():
 	txt = str(txt[i:j+1]).replace("true", "True").replace("false", "False")
 	dic = eval(txt)
 	print dic
+	return dic
+
+def gameday(time):
+	return game[time]
+
+game = [
+	{"hits":0, "runs":0, "on base":0, "errors":0, "hitsOpp":0, "runsOpp":0, "on baseOpp":0, "errorsOpp":0, "winner":None},
+	{"hits":1, "runs":0, "on base":1, "errors":0, "hitsOpp":0, "runsOpp":0, "on baseOpp":0, "errorsOpp":0, "winner":None},
+	{"hits":1, "runs":0, "on base":1, "errors":0, "hitsOpp":0, "runsOpp":0, "on baseOpp":0, "errorsOpp":0, "winner":None},
+	{"hits":1, "runs":1, "on base":0, "errors":0, "hitsOpp":0, "runsOpp":0, "on baseOpp":0, "errorsOpp":0, "winner":None},
+	{"hits":1, "runs":1, "on base":0, "errors":0, "hitsOpp":1, "runsOpp":0, "on baseOpp":0, "errorsOpp":0, "winner":None},
+	{"hits":1, "runs":1, "on base":0, "errors":0, "hitsOpp":1, "runsOpp":0, "on baseOpp":0, "errorsOpp":0, "winner":None},
+	{"hits":1, "runs":1, "on base":0, "errors":0, "hitsOpp":1, "runsOpp":1, "on baseOpp":0, "errorsOpp":0, "winner":None},
+	{"hits":2, "runs":2, "on base":0, "errors":0, "hitsOpp":1, "runsOpp":1, "on baseOpp":0, "errorsOpp":0, "winner":None},
+	{"hits":2, "runs":2, "on base":0, "errors":0, "hitsOpp":1, "runsOpp":1, "on baseOpp":0, "errorsOpp":0, "winner":None},
+	{"hits":2, "runs":2, "on base":0, "errors":0, "hitsOpp":1, "runsOpp":1, "on baseOpp":0, "errorsOpp":0, "winner":"SF"}
+]
 
 if __name__ == "__main__":
 	main()
--- interface.py
+++ interface.py
@@ -4,21 +4,59 @@ import serial
 import time
 
 def main():
-	# ser = serial.Serial('/dev/cu.HC-06-DevB', 9600)
+	ser = serial.Serial('/dev/cu.HC-06-DevB', 9600)
+	lastScore = 0
+	gameTime = 0
+	gameState = {}
+	lastGameState = g.gameday(0)
+	stocks = s.update()
+	num = stocks["GOOG"]["ChangePercent"]
 	while True:
 		print "updating..."
-		stocks = s.update()
-		giants = g.update()
+		# giants = g.update()
+		if gameTime < len(g.game):
+			gameState = g.gameday(gameTime)
 		print "done."
-		time.sleep(5)
-		num = stocks["GOOG"]["ChangePercent"]
-		if giants["winner"]:
-			win = "1"
-		else
-			win = "0"
-		ser.write("%d\n" % win)
-		ser.write("%d\n" % num)
-		print num
+		time.sleep(2)
+		gameTime += 1
+
+		# First write is winner: 0, 1
+		if gameState["winner"] == "SF":
+			ser.write("1")
+		else:
+			ser.write("0")
+
+		# Second write is score change: 0, 1
+		if gameState["runs"] > lastGameState["runs"]:
+			ser.write("%d" % (gameState["runs"] - gameState["runsOpp"]))
+		else:
+			ser.write("0")
+
+		# Third write is hit change: 0, 1
+		if gameState["hits"] > lastGameState["hits"]:
+			ser.write("1")
+		else:
+			ser.write("0")
+
+		# Fourth write is winning or not: 0, 1, 2
+		if gameState["runs"] > gameState["runsOpp"]:
+			ser.write("2")
+		elif gameState["runs"] == gameState["runsOpp"]:
+			ser.write("1")
+		else:
+			ser.write("0")
+
+		# Fifth write is stock change: 0, 1, 2
+		if num < -1:
+			num = 0
+		elif num > 1:
+			num = 2
+		else:
+			num = 1
+		ser.write("%d" % num)
+		print lastGameState
+		print gameState
+		lastGameState = gameState
 
 if __name__ == '__main__':
 	main()
